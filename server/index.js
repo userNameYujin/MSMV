8da8f7b1d04c19aba18965ea01e63a6bffa92ba1
seongmin <psm7335@gmail.com>
2021-05-20 19:57:04 +0900

    passport

--- server/index.js
+++ server/index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const dotenv = require('dotenv');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-
-
+const cors = require('cors');
 const app = express();
+const passport = require('passport');
+const passportConfig = require('./lib/passport');
+passportConfig();
+const MySQLStore = require('express-mysql-session')(session);
+dotenv.config();
+
+const options ={
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE
+}
+const sessionStore = new MySQLStore(options);
 
 app.set('port',process.env.PORT || 8000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
@@ -18,10 +32,25 @@ app.use(session({
     httpOnly: true,
     secure: false,
     sameSite: 'none',
-  }
+  },
+  store: sessionStore
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next)=> {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!');
---
+++ server/passport/index.js
@@ -0,0 +1,20 @@
+const db = require('../lib/db');
+const passport = require('passport');
+const local = require('./localStrategy');
+
+module.exports = () =>{
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+      });
+    
+      passport.deserializeUser(function (id, done) {
+        db.query('SELECT id, user_id, nickname FROM users WHERE id=?', [id], function (err, result) {
+          if (err) {
+            throw err;
+          }
+          done(null, result[0]);
+        });
+      });
+    
+      local();
+}
