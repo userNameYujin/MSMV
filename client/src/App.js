4ff3ed69a8e4b812609ae741547af5b1f7818ef2
odh <kenny022049@naver.com>
2021-06-21 16:57:50 +0900

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React from 'react';
-import Main from './routes/Main';
-import Auth from './routes/Auth';
-import Join from './routes/Join';
-import Header from './routes/Header';
-import Search from './routes/Search';
+
+import Main from './routes/Main.js';
+import Auth from './routes/Auth.js';
+import Join from './routes/join.js';
+import Header from './routes/header.js';
+import Search from './routes/Search.js';
+
+import store from "./store";
+import axios from 'axios';
 
 import { useEffect, useState } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom'; 
-import store from "./store";
-import axios from 'axios';
+
 
 function App() {
   const [isLoginChecked, setIsLoginChecked] = useState(false);
