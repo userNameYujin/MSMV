5e34e8baf745b06c3bc8aebf36aadba1e722fb7b
seongmin <psm7335@gmail.com>
2021-06-29 22:07:33 +0900

    fix post.js

---
+++ server/lib/boxOffice/crawling.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getHTML = async(keyword) => {
+    try{
+        return await axios.get("https://movie.naver.com/movie/bi/mi/basic.nhn?code="+keyword)
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+const boxOfficeParsing = async(keyword, result, callback) => {
+    const html = await getHTML(keyword);
+    
+    const $ = cheerio.load(html.data); 
+
+    result.image = $(".mv_info_area").find("img").attr("src")
+
+    callback(result);
+
+}
+module.exports = {boxOfficeParsing};
