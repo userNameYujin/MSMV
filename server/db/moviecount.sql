7aafb48a37fe11084302aca3b3b9e046c121e9da
Domaya <c2hila@gmail.com>
2021-06-30 13:48:07 +0900

    Add event scheduler to moviecount.sql

--- server/db/moviecount.sql
+++ server/db/moviecount.sql
@@ -3,3 +3,19 @@ create table moviecount(
     count int(10),
     PRIMARY KEY(movieCd)
 );
+
+SET GLOBAL event_scheduler = ON; 
+DELIMITER $$
+CREATE EVENT IF NOT EXISTS CopyCount
+    ON SCHEDULE
+       EVERY 1 DAY STARTS '2021-07-01 00:00:00'
+    ON COMPLETION PRESERVE
+    ENABLE
+    COMMENT 'make a copy'
+    DO
+	BEGIN
+		TRUNCATE TABLE todaymovie;
+		INSERT INTO todaymovie SELECT * FROM moviecount;
+		TRUNCATE TABLE moviecount;
+	END$$
+DELIMITER ;
